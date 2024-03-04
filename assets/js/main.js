feb2815b3bb26f67ef00fba686a3e3ae93b59e09
Maximilian Götz <contact@maxbits.net>
2024-03-04 01:17:27 +0100

    copy snippet feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
 function decodeAndSend(input) {
   window.location.href = `mailto:${atob(input)}`
 }
+
+window.onload = () => {
+  document.querySelectorAll("pre").forEach((item) => {
+    let content = item.innerText
+    let initText = "Copy this snippet"
+    let copyBtn = document.createElement("a")
+
+    copyBtn.innerText = initText
+    copyBtn.href = "javascript:void(0)"
+    copyBtn.classList.add("copybtn")
+
+    copyBtn.onclick = (event) => {
+      event.preventDefault()
+      navigator.clipboard.writeText(content).then(() => {
+        copyBtn.innerText = "Copied!"
+        setTimeout(() => {
+          copyBtn.innerText = initText
+        }, 2000)
+      }, (err) => {
+        console.error(err)
+      })
+    }
+
+    item.parentNode.insertBefore(copyBtn, item.nextSibling)
+  })
+}
