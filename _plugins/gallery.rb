0af824fac5fa8101d602bfeb86bfc4ea79767902
Maximilian Götz <contact@maxbits.net>
2025-03-15 15:36:04 +0100

    create gallery plugin

---
+++ _plugins/gallery.rb
@@ -0,0 +1,11 @@
+module Gallery
+  class AlbumPageGenerator < Jekyll::Generator
+    safe true
+
+    def generate(site)
+      # read albums from db
+      # read fitting photos from db
+      # generate a page for each
+    end
+  end
+end
