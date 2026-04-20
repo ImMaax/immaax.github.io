4af72d12cf5c204979781918fdfe93240d7bf410
Maximilian Götz <contact@maxbits.net>
2026-04-20 22:13:39 +0200

    preparation for galleries

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -29,22 +29,34 @@ function runSlideshow(targetImg, targetDiv, paths) {
     }, 1000)
   }
 
-  setInterval(showNextSlide, 6000)
+  if (paths.length > 1)
+    return setInterval(showNextSlide, 6000)
 }
 
-window.addEventListener("load", () => {
-  const gImg   = document.getElementById("gallery-img")
-  const gSlide = document.getElementById("gallery-slide")
-
-  if (!gImg || !gSlide) return
+function bootHomepage(gImg, gSlide, gList) {
+  let intId = null
 
   fetch("/assets/img/gallery.json", { cache: "no-cache" })
     .then(res => {
       if (!res.ok) {
-        throw new Error(`Error loading gallery! HTTP ${res.status}`);
+        throw new Error(`Error loading gallery! HTTP ${res.status}`)
       }
       return res.json()
     })
-    .then(data => runSlideshow(gImg, gSlide, data))
+    .then(data => {
+      intId = runSlideshow(gImg, gSlide, data)
+    })
     .catch(err => alert(err))
-})
+
+  gList.querySelectorAll("#gallery-list-item").forEach(item => {
+    item.addEventListener("mouseenter", ev => {
+      clearInterval(intId)
+      let newImg = ev.target.getAttribute("data-cover")
+
+      gSlide.classList.add("hidden")
+      setTimeout(() => {
+        intId = runSlideshow(gImg, gSlide, [newImg])
+      }, 1000)
+    })
+  })
+}
